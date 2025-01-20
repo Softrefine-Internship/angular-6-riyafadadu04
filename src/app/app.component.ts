4aa3c9d1a1f9460a96bd6f43cf831dcb5aa53531
riyafadadu04 <riyafadadu2004@gmail.com>
2025-01-20 16:24:49 +0530

    test 6 completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-6';
+export class AppComponent implements OnInit {
+  registerForm!: FormGroup;
+  genders = ['male', 'female'];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      fname: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(50),
+      ]),
+      lname: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(50),
+      ]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      gender: new FormControl('male', [Validators.required]),
+      married: new FormControl(false, [Validators.required]),
+      country: new FormControl('india', [Validators.required]),
+    });
+  }
+
+  OnSubmit() {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+      this.onReset();
+    }
+  }
+
+  onReset() {
+    this.registerForm.reset({
+      fname: null,
+      lname: null,
+      email: null,
+      gender: 'male',
+      married: false,
+      country: 'india',
+    });
+  }
 }
